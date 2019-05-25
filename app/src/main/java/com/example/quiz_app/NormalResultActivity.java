ce5d2981eaa8f5d7009adef3d801ac730ef3122c
usmanasghar78 <osmanasghar78@gmail.com>
2019-05-25 14:19:26 +0500

    Levels Classes & Activities added

---
+++ app/src/main/java/com/example/quiz_app/NormalResultActivity.java
@@ -0,0 +1,13 @@
+package com.example.quiz_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NormalResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_result);
+    }
+}
